2e83b04296aad573ca3b364e4d0976d0068ef2f0
Kuat <80914126+quavodanceq@users.noreply.github.com>
2025-01-20 01:34:49 +0500

    Main screen info view

---
+++ CryptoPool/Views/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  CryptoPool
+//
+//  Created by Куат Оралбеков on 19.01.2025.
+//
+
+import Foundation
---
+++ CryptoPool/Views/ScrollView.swift
@@ -0,0 +1,8 @@
+//
+//  ScrollView.swift
+//  CryptoPool
+//
+//  Created by Куат Оралбеков on 20.01.2025.
+//
+
+import Foundation
